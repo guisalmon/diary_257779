8f59489bafc8039d72d7ba5893f3b4a153254ef0
Guillaume Salmon <guisalmon@gmail.com>
2013-12-15 20:33:43 +0200

    Handling of Entry in database added

--- src/org/rob/diary_257779/classes/Entry.java
+++ src/org/rob/diary_257779/classes/Entry.java
@@ -1,9 +1,44 @@
 package org.rob.diary_257779.classes;
 
 public class Entry {
-
-	public Entry() {
-		// TODO Auto-generated constructor stub
+	private long id;
+	private String title;
+	private String content;
+	private long dateId;
+	private long curDateId;
+	private long diaryId;
+	
+	public Entry(long id, String title, String content, long dateId,
+			long curDateId, long diaryId) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+		this.dateId = dateId;
+		this.curDateId = curDateId;
+		this.diaryId = diaryId;
 	}
-
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public long getDateId() {
+		return dateId;
+	}
+	
+	public long getCurDateId() {
+		return curDateId;
+	}
+	
+	public long getDiaryId() {
+		return diaryId;
+	}
 }
--- src/org/rob/diary_257779/database/DataSource.java
+++ src/org/rob/diary_257779/database/DataSource.java
@@ -3,14 +3,16 @@ package org.rob.diary_257779.database;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.rob.diary_257779.classes.DiaryDate;
 import org.rob.diary_257779.classes.Diary;
+import org.rob.diary_257779.classes.DiaryDate;
+import org.rob.diary_257779.classes.Entry;
 
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.format.Time;
 
 public class DataSource {
 
@@ -22,6 +24,11 @@ public class DataSource {
 			DataBaseContract.CalendarTable.DAY,
 			DataBaseContract.CalendarTable.MONTH,
 			DataBaseContract.CalendarTable.YEAR };
+	private String[] allEntryColumns = { DataBaseContract.EntryTable._ID,
+			DataBaseContract.EntryTable.TITLE,
+			DataBaseContract.EntryTable.DESC, DataBaseContract.EntryTable.DATE,
+			DataBaseContract.EntryTable.CUR_DATE,
+			DataBaseContract.EntryTable.DIARY, };
 
 	public DataSource(Context context) {
 		dbHelper = new DataBaseHelper(context);
@@ -35,14 +42,13 @@ public class DataSource {
 		dbHelper.close();
 	}
 
-	
 	// Methods about Diary
 
 	public Diary createDiary(String name) {
 		ContentValues values = new ContentValues();
 		values.put(DataBaseContract.DiaryTable.NAME, name);
-		long insertId = database.insert(DataBaseContract.DiaryTable.TABLE_NAME, null,
-				values);
+		long insertId = database.insert(DataBaseContract.DiaryTable.TABLE_NAME,
+				null, values);
 		Cursor cursor = database.query(DataBaseContract.DiaryTable.NAME,
 				allDiaryColumns, DataBaseContract.DiaryTable._ID + " = "
 						+ insertId, null, null, null, null);
@@ -55,75 +61,139 @@ public class DataSource {
 
 	public void deleteDiary(Diary diary) {
 		long id = diary.getId();
-	    System.out.println("Comment deleted with id: " + id);
-	    database.delete(DataBaseContract.DiaryTable.TABLE_NAME, DataBaseContract.DiaryTable._ID
-	        + " = " + id, null);
+		database.delete(DataBaseContract.DiaryTable.TABLE_NAME,
+				DataBaseContract.DiaryTable._ID + " = " + id, null);
 	}
-	
+
 	public List<Diary> getAllDiaries() {
 		List<Diary> diaries = new ArrayList<Diary>();
-	    Cursor cursor = database.query(DataBaseContract.DiaryTable.TABLE_NAME,
-	        allDiaryColumns, null, null, null, null, null);
-	    cursor.moveToFirst();
-	    while (!cursor.isAfterLast()) {
-	      Diary diary = cursorToDiary(cursor);
-	      diaries.add(diary);
-	      cursor.moveToNext();
-	    }
-	    cursor.close();
-	    return diaries;
+		Cursor cursor = database.query(DataBaseContract.DiaryTable.TABLE_NAME,
+				allDiaryColumns, null, null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			Diary diary = cursorToDiary(cursor);
+			diaries.add(diary);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return diaries;
 	}
 
 	private Diary cursorToDiary(Cursor cursor) {
 		return new Diary(cursor.getLong(0), cursor.getString(1));
 	}
 
-	
 	// Methods about Calendar
 
-	public DiaryDate createDate(long day, long month, long year){
+	public DiaryDate createDate(long day, long month, long year) {
 		ContentValues values = new ContentValues();
 		values.put(DataBaseContract.CalendarTable.DAY, day);
 		values.put(DataBaseContract.CalendarTable.MONTH, month);
 		values.put(DataBaseContract.CalendarTable.YEAR, year);
-		long insertId = database.insert(DataBaseContract.CalendarTable.TABLE_NAME, null,
-				values);
-		Cursor cursor = database.query(DataBaseContract.CalendarTable.TABLE_NAME,
-				allDateColumns, DataBaseContract.CalendarTable._ID + " = "
-						+ insertId, null, null, null, null);
+		long insertId = database.insert(
+				DataBaseContract.CalendarTable.TABLE_NAME, null, values);
+		Cursor cursor = database.query(
+				DataBaseContract.CalendarTable.TABLE_NAME, allDateColumns,
+				DataBaseContract.CalendarTable._ID + " = " + insertId, null,
+				null, null, null);
 		cursor.moveToFirst();
 		DiaryDate newDiaryDate = cursorToDiaryDate(cursor);
 		cursor.close();
 		return newDiaryDate;
 	}
-	
-	public void deleteDate(DiaryDate date){
+
+	public void deleteDate(DiaryDate date) {
 		long id = date.getId();
-	    System.out.println("Comment deleted with id: " + id);
-	    database.delete(DataBaseContract.CalendarTable.TABLE_NAME, DataBaseContract.CalendarTable._ID
-	        + " = " + id, null);
+		database.delete(DataBaseContract.CalendarTable.TABLE_NAME,
+				DataBaseContract.CalendarTable._ID + " = " + id, null);
 	}
-	
-	public List<DiaryDate> getAllDates(){
+
+	public List<DiaryDate> getAllDates() {
 		List<DiaryDate> dates = new ArrayList<DiaryDate>();
-	    Cursor cursor = database.query(DataBaseContract.CalendarTable.TABLE_NAME,
-	        allDiaryColumns, null, null, null, null, null);
-	    cursor.moveToFirst();
-	    while (!cursor.isAfterLast()) {
-	      DiaryDate date = cursorToDiaryDate(cursor);
-	      dates.add(date);
-	      cursor.moveToNext();
-	    }
-	    cursor.close();
-	    return dates;
+		Cursor cursor = database.query(
+				DataBaseContract.CalendarTable.TABLE_NAME, allDateColumns,
+				null, null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			DiaryDate date = cursorToDiaryDate(cursor);
+			dates.add(date);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return dates;
+	}
+
+	private long getDateId(long day, long month, long year) {
+		String[] idColumn = { DataBaseContract.CalendarTable._ID };
+		Cursor dateCursor = database.query(
+				DataBaseContract.CalendarTable.TABLE_NAME, idColumn,
+				DataBaseContract.CalendarTable.DAY + " = " + day + " AND "
+						+ DataBaseContract.CalendarTable.MONTH + " = " + month
+						+ " AND " + DataBaseContract.CalendarTable.YEAR + " = "
+						+ year, null, null, null, null);
+		if (dateCursor.moveToFirst() != false) {
+			return dateCursor.getLong(0);
+		} else {
+			DiaryDate date = createDate(day, month, year);
+			return date.getId();
+		}
 	}
 
 	private DiaryDate cursorToDiaryDate(Cursor cursor) {
-		return new DiaryDate(cursor.getLong(0), cursor.getLong(1), cursor.getLong(2), cursor.getLong(3));
+		return new DiaryDate(cursor.getLong(0), cursor.getLong(1),
+				cursor.getLong(2), cursor.getLong(3));
 	}
 
-	// TODO
-	// Entry createEntry()
-	// void deleteEntry(Entry entry)
-	// List<Entry> getAllEntries()
+	// Methods about Entry
+
+	public Entry createEntry(String title, String content, long day,
+			long month, long year, Diary diary) {
+		Time now = new Time();
+		now.setToNow();
+
+		ContentValues values = new ContentValues();
+		values.put(DataBaseContract.EntryTable.TITLE, title);
+		values.put(DataBaseContract.EntryTable.DESC, content);
+		values.put(DataBaseContract.EntryTable.DATE,
+				getDateId(day, month, year));
+		values.put(DataBaseContract.EntryTable.CUR_DATE, title);
+		values.put(DataBaseContract.EntryTable.DIARY,
+				getDateId(now.monthDay, now.month, now.year));
+		long insertId = database.insert(DataBaseContract.EntryTable.TABLE_NAME,
+				null, values);
+		Cursor cursor = database.query(DataBaseContract.EntryTable.TABLE_NAME,
+				allEntryColumns, DataBaseContract.EntryTable._ID + " = "
+						+ insertId, null, null, null, null);
+		cursor.moveToFirst();
+		Entry newEntry = cursorToEntry(cursor);
+		cursor.close();
+		return newEntry;
+	}
+
+	public void deleteEntry(Entry entry) {
+		long id = entry.getId();
+		database.delete(DataBaseContract.EntryTable.TABLE_NAME,
+				DataBaseContract.EntryTable._ID + " = " + id, null);
+	}
+
+	public List<Entry> getAllEntries(){
+		List<Entry> entries = new ArrayList<Entry>();
+		Cursor cursor = database.query(
+				DataBaseContract.EntryTable.TABLE_NAME, allEntryColumns,
+				null, null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			Entry entry = cursorToEntry(cursor);
+			entries.add(entry);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return entries;
+	}
+
+	private Entry cursorToEntry(Cursor cursor) {
+		return new Entry(cursor.getLong(0), cursor.getString(1),
+				cursor.getString(2), cursor.getLong(3), cursor.getLong(4),
+				cursor.getLong(5));
+	}
 }
