07e84fcec7e3802782d0713e92ed5c3c6c8108b7
Guillaume Salmon <guisalmon@gmail.com>
2013-12-15 19:38:03 +0200

    Handling on Diary in database added

--- src/org/rob/diary_257779/classes/Diary.java
+++ src/org/rob/diary_257779/classes/Diary.java
@@ -1,9 +1,19 @@
 package org.rob.diary_257779.classes;
 
 public class Diary {
-
-	public Diary() {
-		// TODO Auto-generated constructor stub
+	private long id;
+	private String name;
+	
+	public Diary(long id, String name) {
+		this.id = id;
+		this.name = name;
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
--- src/org/rob/diary_257779/database/DataSource.java
+++ src/org/rob/diary_257779/database/DataSource.java
@@ -1,38 +1,86 @@
 package org.rob.diary_257779.database;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rob.diary_257779.classes.Diary;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class DataSource {
-	
+
 	private SQLiteDatabase database;
 	private DataBaseHelper dbHelper;
-	
+	private String[] allDiaryColumns = { DataBaseContract.DiaryTable._ID,
+			DataBaseContract.DiaryTable.NAME };
+
 	public DataSource(Context context) {
 		dbHelper = new DataBaseHelper(context);
 	}
-	
+
 	public void open() throws SQLException {
-	    database = dbHelper.getWritableDatabase();
+		database = dbHelper.getWritableDatabase();
 	}
 
 	public void close() {
-	    dbHelper.close();
+		dbHelper.close();
+	}
+
+	// Methods about Diary
+
+	public Diary createDiary(String name) {
+		ContentValues values = new ContentValues();
+		values.put(DataBaseContract.DiaryTable.NAME, name);
+		long insertId = database.insert(DataBaseContract.DiaryTable.NAME, null,
+				values);
+		Cursor cursor = database.query(DataBaseContract.DiaryTable.NAME,
+				allDiaryColumns, DataBaseContract.DiaryTable._ID + " = "
+						+ insertId, null, null, null, null);
+		cursor.moveToFirst();
+		Diary newDiary = cursorToDiary(cursor);
+		cursor.close();
+		return newDiary;
+
+	}
+
+	public void deleteDiary(Diary diary) {
+		long id = diary.getId();
+	    System.out.println("Comment deleted with id: " + id);
+	    database.delete(DataBaseContract.DiaryTable.TABLE_NAME, DataBaseContract.DiaryTable._ID
+	        + " = " + id, null);
 	}
 	
-	//TODO
-	//Diary createDiary()
-	//void deleteDiary(Diary diary)
-	//List<Diary> getAllDiaries()
+	public List<Diary> getAllDiaries() {
+		List<Diary> diaries = new ArrayList<Diary>();
+	    Cursor cursor = database.query(DataBaseContract.DiaryTable.TABLE_NAME,
+	        allDiaryColumns, null, null, null, null, null);
+	    cursor.moveToFirst();
+	    while (!cursor.isAfterLast()) {
+	      Diary diary = cursorToDiary(cursor);
+	      diaries.add(diary);
+	      cursor.moveToNext();
+	    }
+	    cursor.close();
+	    return diaries;
+	}
+
+	private Diary cursorToDiary(Cursor cursor) {
+		return new Diary(cursor.getLong(0), cursor.getString(1));
+	}
+
+	// Methods about Calendar
 	
-	//TODO
-	//Calendar createDate()
-	//void deleteDate(Calendar date)
-	//List<Calendar> getAllDates()
-	
-	//TODO
-	//Entry createEntry()
-	//void deleteEntry(Entry entry)
-	//List<Entry> getAllEntries()
+	// TODO
+	// Calendar createDate()
+	// void deleteDate(Calendar date)
+	// List<Calendar> getAllDates()
+
+	// TODO
+	// Entry createEntry()
+	// void deleteEntry(Entry entry)
+	// List<Entry> getAllEntries()
 }
